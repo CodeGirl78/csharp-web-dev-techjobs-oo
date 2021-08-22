05beb6c52d1348b0f220520bd922f9229740f7d4
Melinda <codegirl78@gmail.com>
2021-08-22 17:37:08 -0500

    Added unit test to test full constructor

--- TechJobsTests/JobTests.cs
+++ TechJobsTests/JobTests.cs
@@ -15,5 +15,23 @@ namespace TechJobsTests
             Assert.AreNotEqual(obj1_test.Id, obj2_test.Id);
             Assert.AreEqual(1, obj2_test.Id - obj1_test.Id);
         }
+
+        [TestMethod]
+        public void TestJobConstructorSetsAllFields()
+        {
+            //The constructor correctly assigns the value of each field.
+            Employer ACME = new Employer("ACME");
+            Location Desert = new Location("Desert");
+            PositionType Quality_control = new PositionType("Quality control");
+            CoreCompetency Persistence = new CoreCompetency("Persistence");
+
+            Job obj3_test = new Job("Product tester", ACME, Desert, Quality_control, Persistence);
+
+            Assert.AreEqual("Product tester", obj3_test.Name);
+            Assert.AreEqual(ACME, obj3_test.EmployerName);
+            Assert.AreEqual(Desert, obj3_test.EmployerLocation);
+            Assert.AreEqual(Quality_control, obj3_test.JobType);
+            Assert.AreEqual(Persistence, obj3_test.JobCoreCompetency);
+        }
     }
 }
