776609c78281b8a67f61d89493da6ac28f189535
Melinda <codegirl78@gmail.com>
2021-08-22 17:24:07 -0500

    Added using statement and first unit test

--- TechJobsTests/JobTests.cs
+++ TechJobsTests/JobTests.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechJobsOO;
 
 namespace TechJobsTests
 {
@@ -6,8 +7,13 @@ namespace TechJobsTests
     public class JobTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestSettingJobId()
         {
+            //Each Job object should contain a unique ID number, and these should also be sequential integers.
+            Job obj1_test = new Job();
+            Job obj2_test = new Job();
+            Assert.AreNotEqual(obj1_test.Id, obj2_test.Id);
+            Assert.AreEqual(1, obj2_test.Id - obj1_test.Id);
         }
     }
 }
